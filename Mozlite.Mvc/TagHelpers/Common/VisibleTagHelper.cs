70a1282d138c79f018422cc735a0b55725ab78ca
andy <myxbing@hotmail.com>
2018-03-08 15:03:27 +0800

    添加显示标签。

---
+++ Mozlite.Mvc/TagHelpers/Common/VisibleTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Mozlite.Mvc.TagHelpers.Common
+{
+    /// <summary>
+    /// 显示标签。
+    /// </summary>
+    [HtmlTargetElement("*", Attributes = AttributeName)]
+    public class VisibleTagHelper : TagHelperBase
+    {
+        private const string AttributeName = ".visible";
+        /// <summary>
+        /// 是否显示。
+        /// </summary>
+        [HtmlAttributeName(AttributeName)]
+        public bool IsVisible { get; set; }
+
+        /// <summary>
+        /// 访问并呈现当前标签实例。
+        /// </summary>
+        /// <param name="context">当前HTML标签上下文，包含当前HTML相关信息。</param>
+        /// <param name="output">当前标签输出实例，用于呈现标签相关信息。</param>
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (IsVisible)
+                return;
+            output.SuppressOutput();
+        }
+    }
+}
