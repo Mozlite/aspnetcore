6641fde5e43acea652c29539873c7cc940899cbd
xiaobing <myxbing@hotmail.com>
2018-03-27 20:09:07 +0800

    添加文档项目，构建帮助文档。

---
+++ Mozlite.Extensions.Documents/CategoryDataMigration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Mozlite.Data.Migrations;
+using Mozlite.Data.Migrations.Builders;
+using Mozlite.Extensions.Groups;
+
+namespace Mozlite.Extensions.Documents
+{
+    /// <summary>
+    /// 数据迁移类。
+    /// </summary>
+    public class CategoryDataMigration : GroupDataMigration<Category>
+    {
+        /// <summary>
+        /// 编辑表格其他属性列。
+        /// </summary>
+        /// <param name="table">当前表格构建实例对象。</param>
+		protected override void Create(CreateTableBuilder<Category> table)
+        {
+            table.Column(x => x.Title)
+                 .Column(x => x.Description)
+                 .Column(x => x.Order);
+        }
+	}
+}
---
+++ Mozlite.Extensions.Documents/CategoryManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+using Mozlite.Data;
+using Mozlite.Extensions.Groups;
+
+namespace Mozlite.Extensions.Documents
+{
+    /// <summary>
+    /// 分类管理实现类。
+    /// </summary>
+    public class CategoryManager : GroupManager<Category>, ICategoryManager
+    {
+        /// <summary>
+        /// 初始化类<see cref="CategoryManager"/>。
+        /// </summary>
+        /// <param name="context">数据库操作接口实例。</param>
+        /// <param name="cache">缓存接口。</param>
+        public CategoryManager(IDbContext<Category> context, IMemoryCache cache) 
+            : base(context, cache)
+        {
+        }
+    }
+}
