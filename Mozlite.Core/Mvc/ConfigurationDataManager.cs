3ba76d7709d8d2f8cd8482477a51bf067bcb1377
andy <myxbing@hotmail.com>
2018-07-24 15:37:20 +0800

    添加文件配置功能：configdata文件夹下得json文件。

---
+++ Mozlite.Core/Mvc/ConfigurationDataManager.cs
@@ -0,0 +1,118 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+
+namespace Mozlite.Mvc
+{
+    /// <summary>
+    /// 配置管理。
+    /// </summary>
+    public class ConfigurationDataManager : IConfigurationDataManager
+    {
+        private readonly IMemoryCache _cache;
+        private const string ConfigDir = "configdata";
+
+        private string GetPath(string name)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), ConfigDir);
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            return Path.Combine(path, $"{name}.json");
+        }
+
+        private string GetCacheKey(string name) => $"{ConfigDir}:[{name}]";
+
+        /// <summary>
+        /// 初始化类<see cref="ConfigurationDataManager"/>。
+        /// </summary>
+        /// <param name="cache">缓存接口。</param>
+        public ConfigurationDataManager(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+        
+        /// <summary>
+        /// 加载配置。   
+        /// </summary>
+        /// <typeparam name="TConfiguration">配置类型。</typeparam>
+        /// <param name="name">名称，不包含文件扩展名。</param>
+        /// <param name="minutes">缓存分钟数。</param>
+        /// <returns>返回配置实例。</returns>
+        public virtual TConfiguration LoadConfiguration<TConfiguration>(string name, int minutes = 3)
+        {
+            if (minutes <= 0) return LoadConfiguration<TConfiguration>(name);
+            return _cache.GetOrCreate(GetCacheKey(name), ctx =>
+            {
+                ctx.SetAbsoluteExpiration(TimeSpan.FromMinutes(minutes));
+                return LoadConfiguration<TConfiguration>(name);
+            });
+        }
+
+        private TConfiguration LoadConfiguration<TConfiguration>(string name)
+        {
+            var path = GetPath(name);
+            if (!File.Exists(path))
+                return default;
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var sr = new StreamReader(fs, Encoding.UTF8))
+                return JsonConvert.DeserializeObject<TConfiguration>(sr.ReadToEnd());
+        }
+
+        /// <summary>
+        /// 加载配置。   
+        /// </summary>
+        /// <typeparam name="TConfiguration">配置类型。</typeparam>
+        /// <param name="name">名称，不包含文件扩展名。</param>
+        /// <param name="minutes">缓存分钟数。</param>
+        /// <returns>返回配置实例。</returns>
+        public virtual async Task<TConfiguration> LoadConfigurationAsync<TConfiguration>(string name, int minutes = 3)
+        {
+            if (minutes <= 0) return await LoadConfigurationAsync<TConfiguration>(name);
+            return await _cache.GetOrCreateAsync(GetCacheKey(name), async ctx =>
+            {
+                ctx.SetAbsoluteExpiration(TimeSpan.FromMinutes(minutes));
+                return await LoadConfigurationAsync<TConfiguration>(name);
+            });
+        }
+
+        private async Task<TConfiguration> LoadConfigurationAsync<TConfiguration>(string name)
+        {
+            var path = GetPath(name);
+            if (!File.Exists(path))
+                return default;
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var sr = new StreamReader(fs, Encoding.UTF8))
+                return JsonConvert.DeserializeObject<TConfiguration>(await sr.ReadToEndAsync());
+        }
+
+        /// <summary>
+        /// 保存配置。
+        /// </summary>
+        /// <param name="name">名称，不包含文件扩展名。</param>
+        /// <param name="configuration">配置实例。</param>
+        public virtual void SaveConfiguration(string name, object configuration)
+        {
+            var path = GetPath(name);
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+            using (var sw = new StreamWriter(fs, Encoding.UTF8))
+                sw.Write(JsonConvert.SerializeObject(configuration));
+            _cache.Remove(GetCacheKey(name));
+        }
+
+        /// <summary>
+        /// 保存配置。
+        /// </summary>
+        /// <param name="name">名称，不包含文件扩展名。</param>
+        /// <param name="configuration">配置实例。</param>
+        public virtual async Task SaveConfigurationAsync(string name, object configuration)
+        {
+            var path = GetPath(name);
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+            using (var sw = new StreamWriter(fs, Encoding.UTF8))
+                await sw.WriteAsync(JsonConvert.SerializeObject(configuration));
+            _cache.Remove(GetCacheKey(name));
+        }
+    }
+}
---
+++ Mozlite.Core/Mvc/IConfigurationDataManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+
+namespace Mozlite.Mvc
+{
+    /// <summary>
+    /// JSON配置数据管理接口。
+    /// </summary>
+    public interface IConfigurationDataManager : ISingletonService
+    {
+        /// <summary>
+        /// 加载配置。   
+        /// </summary>
+        /// <typeparam name="TConfiguration">配置类型。</typeparam>
+        /// <param name="name">名称，不包含文件扩展名。</param>
+        /// <param name="minutes">缓存分钟数。</param>
+        /// <returns>返回配置实例。</returns>
+        TConfiguration LoadConfiguration<TConfiguration>(string name, int minutes = 3);
+
+        /// <summary>
+        /// 加载配置。   
+        /// </summary>
+        /// <typeparam name="TConfiguration">配置类型。</typeparam>
+        /// <param name="name">名称，不包含文件扩展名。</param>
+        /// <param name="minutes">缓存分钟数。</param>
+        /// <returns>返回配置实例。</returns>
+        Task<TConfiguration> LoadConfigurationAsync<TConfiguration>(string name, int minutes = 3);
+
+        /// <summary>
+        /// 保存配置。
+        /// </summary>
+        /// <param name="name">名称，不包含文件扩展名。</param>
+        /// <param name="configuration">配置实例。</param>
+        void SaveConfiguration(string name, object configuration);
+
+        /// <summary>
+        /// 保存配置。
+        /// </summary>
+        /// <param name="name">名称，不包含文件扩展名。</param>
+        /// <param name="configuration">配置实例。</param>
+        Task SaveConfigurationAsync(string name, object configuration);
+    }
+}
