57f2486a686a24ee65844d27cfae748cec1c954c
网魂小兵 <myxbing@hotmail.com>
2018-08-31 13:47:30 +0800

    修改为mysql数据库。

---
+++ Mozlite.Data.MySql/Query/Translators/StringReplaceTranslator.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Mozlite.Data.Query.Expressions;
+using Mozlite.Data.Query.Translators;
+
+namespace Mozlite.Data.MySql.Query.Translators
+{
+    /// <summary>
+    /// 字符串替换转换器。
+    /// </summary>
+    public class StringReplaceTranslator : IMethodCallTranslator
+    {
+        private static readonly MethodInfo _methodInfo
+            = typeof(string).GetRuntimeMethod(nameof(string.Replace), new[] { typeof(string), typeof(string) });
+
+        /// <summary>
+        /// 转换表达式。
+        /// </summary>
+        /// <param name="methodCallExpression">方法调用表达式。</param>
+        /// <returns>返回转换后的表达式。</returns>
+        public virtual Expression Translate( MethodCallExpression methodCallExpression)
+        {
+            if (methodCallExpression.Method == _methodInfo)
+            {
+                var sqlArguments = new[] { methodCallExpression.Object }.Concat(methodCallExpression.Arguments);
+                return new SqlFunctionExpression("REPLACE", methodCallExpression.Type, sqlArguments);
+            }
+
+            return null;
+        }
+    }
+}
