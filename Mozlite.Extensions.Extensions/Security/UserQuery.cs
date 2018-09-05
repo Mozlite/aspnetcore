6a885a69f2c4d158aba93b6408b9db7ce1c3dde2
网魂小兵 <myxbing@hotmail.com>
2018-09-05 16:21:52 +0800

    添加用户分页查询。

---
+++ Mozlite.Extensions.Extensions/Security/UserQuery.cs
@@ -0,0 +1,60 @@
+﻿using Mozlite.Data;
+using Mozlite.Extensions.Security.Stores;
+using Mozlite.Extensions.Extensions.Security.Stores;
+
+namespace Mozlite.Extensions.Extensions.Security
+{
+    /// <summary>
+    /// 分页查询基类。
+    /// </summary>
+    /// <typeparam name="TUser">用户类型。</typeparam>
+    public abstract class UserQuery<TUser> : Mozlite.Extensions.Security.UserQuery<TUser>, ISitable
+        where TUser : UserExBase, ISitable
+    {
+        /// <summary>
+        /// 获取当前站Id。
+        /// </summary>
+        public int SiteId { get; set; }
+
+        /// <summary>
+        /// 初始化查询上下文。
+        /// </summary>
+        /// <param name="context">查询上下文。</param>
+        protected override void Init(IQueryContext<TUser> context)
+        {
+            base.Init(context);
+            if (SiteId > 0)
+                context.Where(x => x.SiteId == SiteId);
+        }
+    }
+
+    /// <summary>
+    /// 分页查询基类。
+    /// </summary>
+    /// <typeparam name="TUser">用户类型。</typeparam>
+    /// <typeparam name="TRole">用户组类型。</typeparam>
+    public abstract class UserQuery<TUser, TRole> : UserQuery<TUser>
+        where TUser : UserExBase, ISitable
+        where TRole : RoleBase, ISitable
+    {
+        /// <summary>
+        /// 当前用户用户组等级。
+        /// </summary>
+        public int MaxRoleLevel { get; set; }
+
+        /// <summary>
+        /// 初始化查询上下文。
+        /// </summary>
+        /// <param name="context">查询上下文。</param>
+        protected override void Init(IQueryContext<TUser> context)
+        {
+            base.Init(context);
+            if (MaxRoleLevel > 0)
+            {
+                context.Select()
+                    .LeftJoin<TRole>((u, r) => u.RoleId == r.RoleId)
+                    .Where<TRole>(x => x.RoleLevel < MaxRoleLevel);
+            }
+        }
+    }
+}
---
+++ Mozlite.Extensions/Security/UserQuery.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Mozlite.Data;
+using Mozlite.Extensions.Security.Stores;
+
+namespace Mozlite.Extensions.Security
+{
+    /// <summary>
+    /// 分页查询基类。
+    /// </summary>
+    /// <typeparam name="TUser">用户类型。</typeparam>
+    public abstract class UserQuery<TUser> : QueryBase<TUser>
+        where TUser : UserBase
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 注册开始时间。
+        /// </summary>
+        public DateTime? Start { get; set; }
+
+        /// <summary>
+        /// 注册结束时间。
+        /// </summary>
+        public DateTime? End { get; set; }
+
+        /// <summary>
+        /// 初始化查询上下文。
+        /// </summary>
+        /// <param name="context">查询上下文。</param>
+        protected override void Init(IQueryContext<TUser> context)
+        {
+            if (!string.IsNullOrWhiteSpace(Name))
+                context.Where(x => x.NormalizedUserName.Contains(Name) || x.UserName.Contains(Name));
+            if (Start != null)
+                context.Where(x => x.CreatedDate >= Start);
+            if (End != null)
+                context.Where(x => x.CreatedDate <= End);
+        }
+    }
+    
+    /// <summary>
+    /// 分页查询基类。
+    /// </summary>
+    /// <typeparam name="TUser">用户类型。</typeparam>
+    /// <typeparam name="TRole">用户组类型。</typeparam>
+    public abstract class UserQuery<TUser, TRole> : UserQuery<TUser>
+        where TUser : UserBase
+        where TRole : RoleBase
+    {
+        /// <summary>
+        /// 当前用户用户组等级。
+        /// </summary>
+        public int MaxRoleLevel { get; set; }
+
+        /// <summary>
+        /// 初始化查询上下文。
+        /// </summary>
+        /// <param name="context">查询上下文。</param>
+        protected override void Init(IQueryContext<TUser> context)
+        {
+            base.Init(context);
+            if (MaxRoleLevel > 0)
+            {
+                context.Select()
+                    .LeftJoin<TRole>((u, r) => u.RoleId == r.RoleId)
+                    .Where<TRole>(x => x.RoleLevel < MaxRoleLevel);
+            }
+        }
+    }
+}
