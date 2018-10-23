4c7989ea0f31c3596b16b42aaa4da5ce14fd2613
网魂小兵 <myxbing@hotmail.com>
2018-10-23 11:25:03 +0800

    修改用户代理。

--- Mozlite.Extensions.Storages/MediaDirectory.cs
+++ Mozlite.Extensions.Storages/MediaDirectory.cs
@@ -19,7 +19,7 @@ namespace Mozlite.Extensions.Storages
         private readonly IDbContext<StoredFile> _sfdb;
 
         private const string UserAgent =
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
         private readonly string _media;
 
         /// <summary>
